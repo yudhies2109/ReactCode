1a5b9ef6ec1b650bf08bff6f51fa4f60c8d7e0ec
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-15 16:58:56 +0700

    Class * Hook Counter (difference code)

--- reactbase/src/App.js
+++ reactbase/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+// import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <h2> TEST REACT HOOK </h2>
+      <HookCounter/>
     </div>
   );
 }
---
+++ reactbase/src/components/HookCounter.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function HookCounter() {
+
+    const [count, setcount] = useState(0)
+
+    return (
+        <div>
+            <button onClick={ () => setcount( count + 1 ) }> Count { count }</button>
+        </div>
+    )
+}
+
+export default HookCounter

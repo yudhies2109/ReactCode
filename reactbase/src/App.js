91d5b7ef30dfe4261b17f4f0c8d7038931dd2b38
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-16 23:48:53 +0700

    useState with Object

--- reactbase/src/App.js
+++ reactbase/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo/>
+      <HookCounterThree/>
     </div>
   );
 }

700c506d87600b74c2e95561b36298655626a11b
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-22 06:45:39 +0700

    useState Array

--- reactbase/src/App.js
+++ reactbase/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+// import HookCounterFofrom './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree/>
+      <HookCounterFour/>
     </div>
   );
 }

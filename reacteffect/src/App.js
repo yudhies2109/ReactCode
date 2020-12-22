b7a90953319c077834ac2d8bedc3f18ec62453c3
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-23 05:32:41 +0700

    useeffect after render

---
+++ reacteffect/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HookCounterOne from './components/HookCounterOne';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterOne />
+    </div>
+  );
+}
+
+export default App;

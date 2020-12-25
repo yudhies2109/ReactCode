060879e7884536b95a6eb92b4c05680e138d3e88
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-25 14:31:01 +0700

    useEffect depedency

--- reacteffect/src/App.js
+++ reacteffect/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 // import HookCounterOne from './components/HookCounterOne';
 // import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
+import IntervalHookCounter from './components/IntervalHookCounter';
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer/>
+      <IntervalHookCounter/>
     </div>
   );
 }

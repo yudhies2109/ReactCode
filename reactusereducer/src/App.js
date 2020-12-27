6d740db429cf80e78d7ba3c26b53f3b4e4fb5a62
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-28 06:58:04 +0700

    usereducer state & action simple

---
+++ reactusereducer/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './components/CounterOne';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne/>
+    </div>
+  );
+}
+
+export default App;

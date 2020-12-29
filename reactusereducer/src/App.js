2ffc471616fb184dcf9d889dbb3fe467c75ade4c
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2020-12-29 08:02:59 +0700

    multiple Reducer

--- reactusereducer/src/App.js
+++ reactusereducer/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 // import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CountetTwo';
+import CounterThree from './components/CounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne/> */}
-      <CounterTwo/>
+      {/* <CounterTwo/> */}
+      <CounterThree/>
     </div>
   );
 }

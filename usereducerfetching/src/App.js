6caf79fb25e5468a42498f0ada33b4d115ceafb7
Yudhistira Maulana <ma.yudhistira@hotmail.com>
2021-01-02 17:11:18 +0700

    usereducer data fetching

--- usereducerfetching/src/App.js
+++ usereducerfetching/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import DataFetchingOne from './components/DataFetchingOne';
+// simport DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 function App() {
   return (
     <div className="App">
-      <DataFetchingOne/>
+      <DataFetchingTwo/>
     </div>
   );
 }
